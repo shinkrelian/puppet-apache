a1d7ddb73f3cc3b4abd686d1c396d41be7cb9d0e
root <root@trainingbit2bit1c.mylabserver.com>
2019-09-16 06:47:09 +0000

    Added vhosts define type

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,6 +12,9 @@ class apache (
   Enum["running", "stopped"] $service_ensure,
   String $service_name,
   Boolean $service_enable,
+  String[1] $vhosts_dir,
+  String[1] $vhosts_owner,
+  String[1] $vhosts_group,
 ) {
   contain apache::install
   contain apache::config
