ea989d803ea333ec3925a36868c743d248d4ea5d
root <root@trainingbit2bit1c.mylabserver.com>
2019-09-16 05:06:55 +0000

    Refactored install class for Debian support

---
+++ manifests/params.pp
@@ -0,0 +1,13 @@
+# @summary 
+#   Operating system-related variables (and others)
+class apache::params {
+  $install_ensure = 'present'
+  case $::osfamily {
+    'RedHat': {
+      $install_name = 'httpd'
+    }
+    'Debian': {
+      $install_name = 'apache2'
+    } 
+  }
+} 
