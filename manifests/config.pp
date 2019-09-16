0ce00a579917e8e5f03a59c7618c56d7ca21fd10
root <root@trainingbit2bit1c.mylabserver.com>
2019-09-16 05:45:03 +0000

    Added config class to manage configuration files

---
+++ manifests/config.pp
@@ -0,0 +1,12 @@
+# @summary 
+#   Managed by Apache configuration
+class apache::config {
+  file { 'apache_config':
+    ensure => $apache::config_ensure,
+    path   => $apache::config_path,
+    source => "puppet:///modules/apache/${osfamily}.conf",
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+  } 
+}
